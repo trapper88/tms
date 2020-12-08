cd8fb93489e7851af9e69542eec3a2fcf19ff79d
Yauheni Valchok <irnwlf1988@yandex.ru>
2020-12-08 20:12:43 +0300

    1

---
+++ Homework/src/com/company/lesson4/Computer.java
@@ -0,0 +1,47 @@
+package com.company.lesson4;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Computer {
+
+    private String processor = "Intel";
+    private int ram = 8192;
+    private int rom = 1024000;
+
+    private static int recourse = 3;
+
+    void description() {
+        System.out.println("Processor " + processor);
+        System.out.println("RAM = " + ram);
+        System.out.println("ROM = " + rom);
+        System.out.println("Resource = " + recourse);
+        System.out.println();
+    }
+
+    void turnOn() {
+        if (recourse <= 0) {
+            System.out.println("Ресурс исчерпан");
+            System.exit(0);
+        } else {
+            Random r = new Random();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Для включения введите 1 либо 0");
+            int z = sc.nextInt();
+            int x = r.nextInt(2);
+
+            if (z != x) {
+                recourse--;
+                System.out.println("Комп сгорел. Остаточный ресурс " + recourse);
+                turnOn();
+            } else {
+
+                recourse--;
+                System.out.println("Комп включился. Остаточный ресурс " + recourse);
+            }
+
+        }
+    }
+
+}
---
+++ Homework/src/com/company/lesson4/Main.java
@@ -0,0 +1,10 @@
+package com.company.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Computer comp = new Computer();
+        comp.description();
+        comp.turnOn();
+    }
+}
